223b636e183a85275fe553df9e227b70ae804dd2
basementlivin <epedersen13@gmail.com>
2022-10-14 16:56:01 -0400

    Improve model relationship

--- models/Performer.js
+++ models/Performer.js
@@ -4,6 +4,7 @@ const PerformerSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     performance_count: {type: Number, required: true, default: 0},
+    recordings: [{type: mongoose.Schema.Types.ObjectId, ref: 'RecordingDetails'}],
 },{timestamps: true});
 
 const Performer = mongoose.model("Performer", PerformerSchema);

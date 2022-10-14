a9da171cf6396f558dcb5c5b3f325ed8aab95079
basementlivin <epedersen13@gmail.com>
2022-10-14 09:20:37 -0400

    Edit Performer schema, set emailproperty to unique

--- models/Performer.js
+++ models/Performer.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const PerformerSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    email: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
     performance_count: {type: Number, required: true, default: 0},
 },{timestamps: true});
 
